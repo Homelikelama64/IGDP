348612877a880dddefb89688f2da0c7b20c2d054
Homelikelama64 <homelikelama64@gmail.com>
2023-06-15 18:46:55 +1200

    Initial commit

---
+++ src/client/java/net/squirrelify/igdp/IGDPClient.java
@@ -0,0 +1,10 @@
+package net.squirrelify.igdp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IGDPClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
---
+++ src/main/java/net/squirrelify/igdp/IGDP.java
@@ -0,0 +1,15 @@
+package net.squirrelify.igdp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IGDP implements ModInitializer {
+    public static final String MOD_ID = "igdp";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
